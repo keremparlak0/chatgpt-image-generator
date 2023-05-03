053ce9bdd8fcbe445c1b67b28af6541336c87178
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-05-03 23:02:26 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const API_KEY = "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";
+const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.querySelector("input");
+const imageSection = document.querySelector(".images-section")
+
+const getImages = async () => {
+    console.log(inputElement.value);
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "prompt": inputElement.value,
+            "n": 2,
+            "size": "1024x1024"
+        })
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+
+    data?.data.forEach(imageObject => {
+        const ImageContainer = document.createElement('div')
+        ImageContainer.classList.add('image-container')
+        const imageElement = document.createElement('img')
+        imageElement.setAttribute('src', imageObject.url)
+        ImageContainer.append(imageElement) 
+        imageSection.append(ImageContainer)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+submitIcon.addEventListener("click", getImages);
